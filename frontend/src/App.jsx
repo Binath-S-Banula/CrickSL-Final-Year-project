b29445defc68a3459f7e96aceea51e1572883ef8
BanulaRPBS <bsbanula@gmail.com>
2026-05-10 00:28:37 +0530

    gave permissions to players to access players dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import { AuthProvider } from './context/AuthContext'
-import ProtectedRoute from './components/ProtectedRoute'
-import Navbar from './components/Navbar'
-import Login from './pages/Login'
-import Home from './pages/Home'
-import VenueWeather from './pages/VenueWeather'
-import PlayingXI from './pages/PlayingXI'
-import DLSCalculator from './pages/DLSCalculator'
-import AdminPanel from './pages/AdminPanel'
-import PlayerDashboard from './pages/PlayerDashboard'
-import Reports from './pages/Reports'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Navbar from "./components/Navbar";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import VenueWeather from "./pages/VenueWeather";
+import PlayingXI from "./pages/PlayingXI";
+import DLSCalculator from "./pages/DLSCalculator";
+import AdminPanel from "./pages/AdminPanel";
+import PlayerDashboard from "./pages/PlayerDashboard";
+import Reports from "./pages/Reports";
 
 export default function App() {
   return (
@@ -17,22 +17,26 @@ export default function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/login" element={<Login />} />
-          <Route path="/*" element={
-            <ProtectedRoute>
-              <Navbar />
-              <Routes>
-                <Route path="/"              element={<Home />} />
-                <Route path="/venue-weather" element={<VenueWeather />} />
-                <Route path="/playing-xi"    element={<PlayingXI />} />
-                <Route path="/dls"           element={<DLSCalculator />} />
-                <Route path="/reports"       element={<Reports />} />
-                <Route path="/admin"         element={<AdminPanel />} />
-                <Route path="*"              element={<Navigate to="/" replace />} />
-              </Routes>
-            </ProtectedRoute>
-          } />
+          <Route
+            path="/*"
+            element={
+              <ProtectedRoute>
+                <Navbar />
+                <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/venue-weather" element={<VenueWeather />} />
+                  <Route path="/playing-xi" element={<PlayingXI />} />
+                  <Route path="/dls" element={<DLSCalculator />} />
+                  <Route path="/reports" element={<Reports />} />
+                  <Route path="/admin" element={<AdminPanel />} />
+                  <Route path="/players" element={<PlayerDashboard />} />
+                  <Route path="*" element={<Navigate to="/" replace />} />
+                </Routes>
+              </ProtectedRoute>
+            }
+          />
         </Routes>
       </BrowserRouter>
     </AuthProvider>
-  )
+  );
 }
